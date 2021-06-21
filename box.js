553dd07882181cf79b3bbdf46175df0da061c0c6
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-21 17:16:44 +0530

    Update box.js

--- box.js
+++ box.js
@@ -1,32 +1,18 @@
 class Box
   {
-    constructor(x,y,w,h,vx)
+    constructor(x,y,w,h)
     {
       this.x =x;
       this.y =y;
       this.w =w;
       this.h = h;
-      this.vx = vx;
     }
 
     show()
   {
     rect(this.x,this.y,this.w,this.h)
   }
-  
-  move()
-  {
-    this.x = this.x+this.vx;
-  }
-
-   bounce()
-  {
-    if (this.x>=width || this.x<=0)
-      {
-        this.vx = -this.vx;
-      }
-  }
-  
+    
   }
 
   
