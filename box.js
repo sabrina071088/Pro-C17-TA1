2ac5644569220c245728d7bd958c1d0b39c9b306
sabrina071088 <73609836+sabrina071088@users.noreply.github.com>
2021-06-26 13:59:11 +0530

    Update box.js

--- box.js
+++ box.js
@@ -6,15 +6,22 @@ class Box
       this.y =y;
       this.w =w;
       this.h = h;
+      this.vx=vx
     }
 
     show()
   {
     rect(this.x,this.y,this.w,this.h)
   }
-    
+    move(){
+      this.x=this.x+this.vx;
   }
+    bounce(){
+      if(this.x>=width || this.x<=0){
+        this.vx=-this.vx
+      }
+    }
 
-  
+  }
   
  
